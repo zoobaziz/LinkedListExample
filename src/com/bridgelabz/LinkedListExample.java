72dfc27ba4f5e5898e4640d300660dc189adc631
jhanvi006 <jhanvikankhara06@gmail.com>
2022-05-05 15:46:10 -0400

    Added: feature to add linked list node at start of the list is added

--- src/com/bridgelabz/LinkedListExample.java
+++ src/com/bridgelabz/LinkedListExample.java
@@ -1,24 +1,35 @@
 package com.bridgelabz;
 
-class Node{
-    public int data;
-    public Node next;
-    public Node(int data){
-        this.data = data;
-        this.next = null;
-    }
-}
 public class LinkedListExample {
-    public static void main(String[] args) {
-        Node firstNode = new Node(56);
-        Node secondNode = new Node(30);
-        Node thirdNode = new Node(70);
-        firstNode.next = secondNode;
-        secondNode.next = thirdNode;
-        Node temp = firstNode;
-        while (temp != null){
-            System.out.print(temp.data+" ");
-            temp = temp.next;
+    static Node head;
+    static Node tail;
+    public static void addAtStart(int data){
+        Node newNode = new Node(data);
+        if (head == null){
+            head = newNode;
+            tail = newNode;
         }
+        else {
+            Node temp = head;
+            head = newNode;
+            newNode.next = temp;
+        }
+    }
+    public static void print(){
+        if (head == null)
+            System.out.println("Linked list is empty.");
+        else{
+            Node temp = head;
+            while (temp != null){
+                System.out.print(temp.data+" ");
+                temp = temp.next;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        addAtStart(70);
+        addAtStart(30);
+        addAtStart(56);
+        print();
     }
 }
