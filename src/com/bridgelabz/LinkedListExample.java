8c2cae476d0ab9e8bca3fcecf2e9f61ceb14a828
Zoobaziz <jayakarabhi@gmail.com>
2022-05-07 15:42:30 +0530

    Update LinkedListExample.java

--- src/com/bridgelabz/LinkedListExample.java
+++ src/com/bridgelabz/LinkedListExample.java
@@ -6,6 +6,7 @@ public class LinkedListExample {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int choice;
+        LinkedList linkedList = LinkedList.getInstance();
         do {
             System.out.println("\nSelect option. \n1. Add data at start. \n2. Add data at end. " +
                     "\n3. Add data in between of two nodes. \n4. Delete first node. " +
@@ -13,19 +14,19 @@ public class LinkedListExample {
             choice = sc.nextInt();
             switch (choice) {
                 case 1:
-                    LinkedList.addDataAtStart();
+                    linkedList.addDataAtStart();
                     break;
                 case 2:
-                    LinkedList.addDataAtEnd();
+                    linkedList.addDataAtEnd();
                     break;
                 case 3:
-                    LinkedList.addDataInMiddle();
+                    linkedList.addDataInMiddle();
                     break;
                 case 4:
-                    LinkedList.removeFirstElement();
+                    linkedList.removeFirstElement();
                     break;
                 case 5:
-                    LinkedList.removeLastElement();
+                    linkedList.removeLastElement();
                     break;
                 default:
                     System.out.println("Invalid number!");
